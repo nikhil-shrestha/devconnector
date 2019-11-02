2db629589591407c2d48ecac34513d0b8579025b
illusionist <nikhil.shrestha1995@gmail.com>
2019-11-02 11:41:05 +0545

    load user & set auth-token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
@@ -10,10 +10,19 @@ import Alert from './components/layout/Alert';
 // Reudx
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
