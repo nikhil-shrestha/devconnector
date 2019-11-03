daa639dd8d9d80296d8760ad3b9b60ac2d33932e
illusionist <nikhil.shrestha1995@gmail.com>
2019-11-03 17:16:17 +0545

    post item component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 
 import { getPosts } from '../../actions/post';
 
@@ -10,7 +11,37 @@ const Posts = ({ post: { posts, loading }, getPosts }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <Fragment>Posts</Fragment>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 class="large text-primary">Posts</h1>
+      <p class="lead">
+        <i class="fas fa-user"></i> Welcome to the community!
+      </p>
+      <div class="post-form">
+        <div class="bg-primary p">
+          <h3>Say Something...</h3>
+        </div>
+        <form class="form my-1">
+          <textarea
+            name="text"
+            cols="30"
+            rows="5"
+            placeholder="Create a post"
+            required
+          ></textarea>
+          <input type="submit" class="btn btn-dark my-1" value="Submit" />
+        </form>
+      </div>
+
+      <div class="posts">
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
